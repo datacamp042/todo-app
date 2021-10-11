158e1442c4cc4e6f9479c4dd69ebdc0df3dac339
datacamp042 <ubuntu@ip-172-31-4-216.eu-west-1.compute.internal>
2021-10-11 14:17:59 +0000

    Bug fix

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -11,7 +11,7 @@ const logger = createLogger('createTodo')
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Processing event: ', event)
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
-
+  
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
@@ -26,7 +26,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       body: JSON.stringify({message: "Name of a task could not be empty"})
     }
   }
-
+  
   const newItem = await createTodo(newTodo, jwtToken)
 
   return {
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ek7kebqetg'
+const apiId = 'nptn3fxazi'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
